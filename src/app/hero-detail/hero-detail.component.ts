050c912f1d076fb9da9fb7981cbecb4f5548c36c
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-27 12:05:51 +0200

    Comments

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -7,6 +7,7 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
+  //Value will be passed on component include
   @Input() selectedHero?: Hero;
 
   constructor() {}
