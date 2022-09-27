fc45ef48564dd750c321b3f51035f08706e67dc2
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-27 11:25:48 +0200

    Added the mock data to the page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes', //The component's element selector
@@ -8,10 +9,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
+  heroes = HEROES;
 
   constructor() { }
 
