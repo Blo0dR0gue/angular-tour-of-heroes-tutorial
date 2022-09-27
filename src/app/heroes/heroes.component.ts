e9b04f66d0d856600cf24611cb0fd53a4392c655
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-27 14:23:12 +0200

    Use service in heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes', //The component's element selector
   templateUrl: './heroes.component.html', //The location of the component's template file
-  styleUrls: ['./heroes.component.css'] //The location of the component's private CSS styles
+  styleUrls: ['./heroes.component.css'], //The location of the component's private CSS styles
 })
 export class HeroesComponent implements OnInit {
-
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+    this.getHeroes();
+  }
 
   //Lifecycle hook - Called after creating the component
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSelect(hero: Hero): void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
