a103f9d51d3b1338d5f5e9d81e5a5e4c965c8005
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-27 10:43:21 +0200

    Added heroes component to app page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes', //The component's element selector
+  templateUrl: './heroes.component.html', //The location of the component's template file
+  styleUrls: ['./heroes.component.css'] //The location of the component's private CSS styles
 })
 export class HeroesComponent implements OnInit {
 
+  hero = 'Windstorm';
+
   constructor() { }
 
+  //Lifecycle hook - Called after creating the component
   ngOnInit(): void {
   }
 
