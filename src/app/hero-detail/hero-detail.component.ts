45d7a8f68bb14b05a8ae57b56f69f992746c7966
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-27 15:21:18 +0200

    Extended hero service to fetch one hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,9 +11,21 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
   //Value will be passed on component include
-  @Input() selectedHero?: Hero;
+  //@Input() selectedHero?: Hero;
+  selectedHero?: Hero;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.selectedHero = hero);
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,15 +5,20 @@ import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);  //Simulate a http call
-    this.messageService.add("HeroService: fetched heroes");
+    const heroes = of(HEROES); //Simulate a http call
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id);
+    this.messageService.add(`HeroService: fetched hero id=${hero!.id}`);
+    return of(hero!);
   }
 }
